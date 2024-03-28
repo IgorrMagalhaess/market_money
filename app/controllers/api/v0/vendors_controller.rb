f76e85296479f673fb36ce554afc4b5756365b0d
Martin Chavez <martinchavez1203@gmail.com>
2024-03-27 22:15:44 -0600

    add show, create, update , destroy actions in vendors controller

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,6 +1,4 @@
 class Api::V0::VendorsController < ApplicationController
-   rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
-
    def index
       market = Market.find(params[:market_id])
       vendors = market.vendors
@@ -12,9 +10,27 @@ class Api::V0::VendorsController < ApplicationController
      render json: VendorSerializer.new(vendor)
    end
 
-   private
-   def not_found_response(exception)
-      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
-         .serialize_json, status: :not_found
+   def create
+      begin
+         vendor = Vendor.create!(vendor_params)
+         
+         render json: VendorSerializer.new(vendor), status: :created 
+      rescue ActiveRecord::RecordInvalid => error
+         render json: ErrorSerializer.new(ErrorMessage.new(error.message, 400)).serialize_json, status: 400
+      end 
+   end
+
+   def update 
+     vendor = Vendor.find(params[:id]) 
+     vendor.update!(vendor_params)
+
+     render json: VendorSerializer.new(vendor), status: 200
+   end
+
+   def destroy 
+     vendor = Vendor.find(params[:id]) 
+      #   vendor.delete(vendor.id)
+      vendor.destroy
+     render json: VendorSerializer.new(vendor), status: 204
    end
 end
