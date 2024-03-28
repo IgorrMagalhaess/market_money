92073d094b988b99995d2527ea881a4488b4f84a
Martin Chavez <martinchavez1203@gmail.com>
2024-03-27 22:14:17 -0600

    add rescues and private methods to be used in all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  rescue_from  ActiveRecord::RecordInvalid, with: :bad_request_response
+
+  private
+
+   def not_found_response(exception)
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+         .serialize_json, status: :not_found
+   end
+
+   def bad_request_response(exception)
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 400))
+         .serialize_json, status: :bad_request
+   end
+
+   def vendor_params
+      params.require(:vendor).permit(:name, :description, :contact_name, :contact_phone, :credit_accepted )
+   end
 end
